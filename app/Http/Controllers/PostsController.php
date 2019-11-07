210aa9855f9036e1efb5755de12cd59ae4f4a59d
Mike Setzer <michaelsetzerucf@gmail.com>
2019-11-07 13:19:58 -0500

    DB Finished

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,25 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Post;
+
 class PostsController
 {
     public function show($slug)
     {
-
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
-//    $posts = [
-//      'my-first-post' => 'Hello, this is my first blog post!',
-//      'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-//    ];
-
-//        if (! array_key_exists($post, $posts)) {
-//            abort(404, 'Sorry, that post was not found.');
-//        }
-
         return view ('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
-
     }
 }
